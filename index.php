d989eb8e56cf0a418e5806bbb88518823177b9b4
Ramazan <ramzan_dakiev@mail.ru>
2021-01-29 15:15:01 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <?php 
+        $name = 'Ramazan';
+        echo "Hello! My name is '$name'." ; 
+        $age = '23';
+        echo "<br>" . "I'm $age." . "<hr>";
+        
+        $a = 12;
+        $b = 32;
+        $rez = $a + $b;
+        echo "$a + $b = $rez" . '<br>';
+
+        $a = $a + $b;
+        $b = $a - $b;
+        $a = $a - $b;
+        echo "$a + $b = $rez" . "<hr>";
+    ?>
+    <div class="first-example">
+        <p>Что такое арбуз?</p>
+        <div class="block">
+            <input type="radio" name="first-answer">
+            <label for="first-answer">Овощ</label>
+        </div>
+        <div class="block">
+            <input type="radio" name="first-answer">
+            <label for="first-answer">Фрукт</label>
+        </div>
+        <div class="block">
+            <input type="radio" name="first-answer">
+            <label for="first-answer">Ягода</label>
+        </div>
+        <div class="block">
+            <input type="radio" name="first-answer">
+            <label for="first-answer">Цветок</label>
+        </div>
+    </div>
+
+    <div class="second-example">
+        <p>фывфы?</p>
+        <div class="block">
+            <input type="checkbox" name="second-answer">
+            <label for="second-answer">ыфвфы</label>
+        </div>
+        <div class="block">
+            <input type="checkbox" name="second-answer">
+            <label for="second-answer">уцууйц</label>
+        </div>
+        <div class="block">
+            <input type="checkbox" name="second-answer">
+            <label for="second-answer">asadaaddasd</label>
+        </div>
+        <div class="block">
+            <input type="checkbox" name="second-answer">
+            <label for="second-answer">asdasd</label>
+        </div>
+    </div>
+
+    <div class="third-example">
+        <p>Кто ты?</p>
+        <textarea></textarea>
+    </div>
+    
+
+    <?php 
+    $background_color = "blue";
+    $color = "red";
+    $width = "100px";
+    $height = "100px";
+    $tag = "style=\"background-color: $background_color; color: $color; width: $width; height: $height;\" ";
+    echo "<hr>"  .  "<div $tag >Hello! </div>" . "<hr>" ;
+    ?>
+    
+   
+</body>
+</html>
