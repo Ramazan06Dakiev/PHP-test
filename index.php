0fafbae35b053b51829b501550f9090c41867961
Ramazan <ramzan_dakiev@mail.ru>
2021-03-18 15:39:40 +0300

    выполнены все задания дз4

--- index.php
+++ index.php
@@ -6,76 +6,94 @@
     <title>PHP</title>
 </head>
 <body>
+
     <?php 
-        $name = 'Ramazan';
-        echo "Hello! My name is '$name'." ; 
-        $age = '23';
-        echo "<br>" . "I'm $age." . "<hr>";
+
+    // №1
+    $style = "style=\"width: 500px;\"";
+    $random = [];
+    for($i = 0; $i  < 100; $i++){
+        $random[$i] = rand(1, 300);
+       
+        $content = implode(" ", $random);
+        $max = max($random);
+        $min = min($random);
+        } 
+        echo  "<p $style> $content</p> ";
+        echo  "<p>Maximum: $max</p> ";
+        echo  "<p>Minimum: $min</p> ";
         
-        $a = 12;
-        $b = 32;
-        $rez = $a + $b;
-        echo "$a + $b = $rez" . '<br>';
 
-        $a = $a + $b;
-        $b = $a - $b;
-        $a = $a - $b;
-        echo "$a + $b = $rez" . "<hr>";
-    ?>
-    <div class="first-example">
-        <p>Что такое арбуз?</p>
-        <div class="block">
-            <input type="radio" name="first-answer">
-            <label for="first-answer">Овощ</label>
-        </div>
-        <div class="block">
-            <input type="radio" name="first-answer">
-            <label for="first-answer">Фрукт</label>
-        </div>
-        <div class="block">
-            <input type="radio" name="first-answer">
-            <label for="first-answer">Ягода</label>
-        </div>
-        <div class="block">
-            <input type="radio" name="first-answer">
-            <label for="first-answer">Цветок</label>
-        </div>
-    </div>
+        echo "<hr>";
 
-    <div class="second-example">
-        <p>Выберите фрукты?</p>
-        <div class="block">
-            <input type="checkbox" name="second-answer">
-            <label for="second-answer">Помидор</label>
-        </div>
-        <div class="block">
-            <input type="checkbox" name="second-answer">
-            <label for="second-answer">Апельсин</label>
-        </div>
-        <div class="block">
-            <input type="checkbox" name="second-answer">
-            <label for="second-answer">Ананас</label>
-        </div>
-        <div class="block">
-            <input type="checkbox" name="second-answer">
-            <label for="second-answer">Клубника</label>
-        </div>
-    </div>
+        // №2
 
-    <div class="third-example">
-        <p>Перечисли моря?</p>
-        <textarea></textarea>
-    </div>
+        $arr = scandir('./img');
+        foreach($arr as $v) {
+        if(stripos($v,'.jpg')) 
+        echo '<img src="img/'.$v.'" width="250" />';
+        }
+        
+        echo "<hr>";
+        // №3
+
+
+        echo "<table cellpadding=\"5\" style=\"display: flex;\">";
+
+        for ($i = 2; $i <= 10; $i++ ) {
+            echo "<tbody>";
+            for ($k = 2; $k <= 10; $k++) {
+                echo "<tr><td align=\"right\">$i * $k = " . $i * $k . "</td></tr>";
+            }
+        }
+        echo "</tbody>";
+        echo "</table>";
+        
+
+        echo "<hr>";
+
+        //№4
+
+        echo "<table>";
+        echo "<tbody>";
+        echo "<tr style=\"background-color: red;\">";
+        echo "<td>Year</td>";
+        echo "<td>Summ start</td>";
+        echo "<td>Summ end</td>";
+        echo "<td>Profit</td>";
+        echo "</tr>";
+
+        $x = 300;
+        $y = 20;
+
+        for ($i = 1; $i <= 20; $i++) {
+            $z = $x + ($x * ($y / 100));
+            $s = $z - $x;
+            
+            echo "<tr style=\"background-color:grey; color: white;\">";
+            echo "<td>$i</td>";
+            echo "<td>" . round($x, 2) . "</td>";
+            echo "<td>" . round($z, 2) . "</td>";
+            echo "<td>" . round($s, 2) . "</td>";
+            echo "</tr>";
+            $x = $z;
+        }
+        
+        echo "</table>";
+        echo "</tbody>";
+
+
+        echo "<hr>";
+        // №5
     
+        $firstNum = 347689;
+        $revNum = strrev($firstNum);
 
-    <?php 
-    $background_color = "blue";
-    $color = "red";
-    $width = "100px";
-    $height = "100px";
-    $tag = "style=\"background-color: $background_color; color: $color; width: $width; height: $height;\" ";
-    echo "<hr>"  .  "<div $tag >Hello! </div>" . "<hr>";
+        echo "<h1 style=\"color: red\";>$firstNum</h1>";
+        echo "<h1 style=\"color: green\";>$revNum</h1>";
+        
     ?>
+    
     
    
 </body>
