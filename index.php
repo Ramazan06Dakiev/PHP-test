8119db8ee859c1e61ee9f7938f57df5292a540b9
Ramazan <ramzan_dakiev@mail.ru>
2021-03-28 16:12:45 +0300

    Выполнены 1-4 задания Task-6

--- index.php
+++ index.php
@@ -7,75 +7,119 @@
 </head>
 <body>
     <?php 
-        $name = 'Ramazan';
-        echo "Hello! My name is '$name'." ; 
-        $age = '23';
-        echo "<br>" . "I'm $age." . "<hr>";
+
+    // №1
+        function Math($x, $y, $z) {
+
+            if ($z == '+') {
+                $summ = $x + $y;
+                echo "<p>$x + $y = $summ</p>";
+
+            } else if ($z == '-') {
+                $diff = $x * $y;
+                echo "<p>$x - $y = $diff</p>";
+                
+            } else if ($z == '*') {
+                $multi = $x * $y; 
+                echo "<p>$x * $y = $multi </p>";
+
+            } else if ($z == '/') {
+
+                if ($y !== 0) {
+                    $div = $x / $y;
+                    echo "<p>$x / $y = $div</p>";
+                } else {
+                    echo "<p>$x / $y = <span style=\"color:red\">DivideByZero Error!</span></p>";
+                }
+               
+            } else {
+                echo "<p>$x  $z  $y   =  <span style=\"color:red\">IncorrectOperation Error!</span></p>";
+            }
+        }
+
+        echo Math(12, 4, '+');
+        echo Math(23, 12, '-');
+        echo Math(2, 5, '*');
+        echo Math(22, 0, '/');
+        echo Math(32, 21, '@');
+
+        //№2
+       
+        function Content($teg, $element) {
+            echo "<$teg class=\"$teg\">$element</$teg>";
+            echo "<style> 
+        h2 {
+            color: blue;
+            padding: 20px;
+            background-color: green;
+        }
+        h3 {
+            color: yellow;
+            padding: 20px;
+            background-color: red;
+        }    
         
-        $a = 12;
-        $b = 32;
-        $rez = $a + $b;
-        echo "$a + $b = $rez" . '<br>';
+        </style>";
+        }
+        
+        echo Content(h3, 'HTML-element');
+        echo Content(h2, 'HTML-element');
 
-        $a = $a + $b;
-        $b = $a - $b;
-        $a = $a - $b;
-        echo "$a + $b = $rez" . "<hr>";
-    ?>
-    <div class="first-example">
-        <p>Что такое арбуз?</p>
-        <div class="block">
-            <input type="radio" name="first-answer">
-            <label for="first-answer">Овощ</label>
-        </div>
-        <div class="block">
-            <input type="radio" name="first-answer">
-            <label for="first-answer">Фрукт</label>
-        </div>
-        <div class="block">
-            <input type="radio" name="first-answer">
-            <label for="first-answer">Ягода</label>
-        </div>
-        <div class="block">
-            <input type="radio" name="first-answer">
-            <label for="first-answer">Цветок</label>
-        </div>
-    </div>
+        //№3
 
-    <div class="second-example">
-        <p>Выберите фрукты?</p>
-        <div class="block">
-            <input type="checkbox" name="second-answer">
-            <label for="second-answer">Помидор</label>
-        </div>
-        <div class="block">
-            <input type="checkbox" name="second-answer">
-            <label for="second-answer">Апельсин</label>
-        </div>
-        <div class="block">
-            <input type="checkbox" name="second-answer">
-            <label for="second-answer">Ананас</label>
-        </div>
-        <div class="block">
-            <input type="checkbox" name="second-answer">
-            <label for="second-answer">Клубника</label>
-        </div>
-    </div>
+        $arrayOne = ['Home', 'About', 'Contact', 'Photo', 'Blog'];
+        $arraySecond = ['Главная', 'О нас', 'Контакты', 'Фото', 'Блог'];
 
-    <div class="third-example">
-        <p>Перечисли моря?</p>
-        <textarea></textarea>
-    </div>
+       
+        
+
+        function Menu($array, $className) {
+        echo "<ul style=\"list-style:none;\">";
+           for ($i = 0; $i < count($array); $i++) { 
+               echo "<li class=\"$className\">" . $array[$i] . "</li>";
+            }; 
+        echo "<style> 
+        .$className {
+            color: yellow;
+            padding: 30px;
+            background-color: green;
+            float: left;
+            cursor: pointer;
+            margin-bottom: 20px;
+        }
+        .$className:hover {
+            color: green;
+            background-color: yellow;
+        }
+        
+        </style>";
+        
+        echo "</ul>";          
+        }
+        
+        echo Menu($arrayOne, 'item');
+        echo Menu($arraySecond, 'item');
+
+        // №4
+
+        function random_html_color()
+        {
+            return sprintf( '#%02X%02X%02X', rand(0, 255), rand(0, 255), rand(0, 255) );
+        }
     
+        while ($a < 5) {
+           $a++;
+           $div = '<div style="background-color:'.random_html_color().'; width:100px; height:100px; margin:10px; display: inline-block;"></div>';
+           echo $div;
+        }
+        
+        //№5
 
-    <?php 
-    $background_color = "blue";
-    $color = "red";
-    $width = "100px";
-    $height = "100px";
-    $tag = "style=\"background-color: $background_color; color: $color; width: $width; height: $height;\" ";
-    echo "<hr>"  .  "<div $tag >Hello! </div>" . "<hr>";
+        
+             
+        
     ?>
+   
     
    
 </body>
